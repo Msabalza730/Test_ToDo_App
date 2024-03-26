ff64870a7250fcb8ab9223b27f027b34f3a33552
MSabalza730 <maryori_sm730@hotmail.com>
2024-03-26 17:52:40 -0500

    [REFACTOR] Refactor database

--- back/app_backend/settings.py
+++ back/app_backend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'app_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_todoapp',
+        'USER': 'user',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
