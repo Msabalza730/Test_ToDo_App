4a2d4c843e2a1391b71750c19d24b387d6e85c5e
MSabalza730 <maryori_sm730@hotmail.com>
2024-03-27 12:10:58 -0500

    [Feat] Adding Login

---
+++ front/src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { login } from './auth';
+
+const Login = () => {
+  const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+    history.push('/tasks');
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
